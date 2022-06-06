f2c8c8149dbce75667be752b74e09be27e7c802a
radian628 <70352880+radian628@users.noreply.github.com>
2022-06-06 16:16:30 -0700

    fixed token gui merge conflict

--- public/precompiled-templates/Token.js
+++ public/precompiled-templates/Token.js
@@ -19,11 +19,11 @@ Handlebars.partials["Token"] = Handlebars.template({"1":function(container,depth
 
   return "<div class=\"token-main\">\r\n    <div class=\"token-name\">\r\n        <label for=\"token-name-input\"> "
     + alias4(((helper = (helper = lookupProperty(helpers,"TokenName") || (depth0 != null ? lookupProperty(depth0,"TokenName") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"TokenName","hash":{},"data":data,"loc":{"start":{"line":3,"column":39},"end":{"line":3,"column":52}}}) : helper)))
-    + " </label>\r\n        <input type=\"text\" id=\"tokenname-input\">\r\n    </div>\r\n\r\n    <div class=\"token-nickname\">\r\n        <label for=\"token-nickname-input\"> "
+    + " </label>\r\n        <input type=\"text\" id=\"token-name-input\">\r\n    </div>\r\n\r\n    <div class=\"token-nickname\">\r\n        <label for=\"token-nickname-input\"> "
     + alias4(((helper = (helper = lookupProperty(helpers,"TokenNickname") || (depth0 != null ? lookupProperty(depth0,"TokenNickname") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"TokenNickname","hash":{},"data":data,"loc":{"start":{"line":8,"column":43},"end":{"line":8,"column":60}}}) : helper)))
-    + " </label>\r\n        <input type=\"text\" id=\"tokenname-input\">\r\n    </div>\r\n    <div class=\"description\">\r\n        <label for=\"token-description-input\"> "
+    + " </label>\r\n        <input type=\"text\" id=\"token-nickname-input\">\r\n    </div>\r\n    <div class=\"description\">\r\n        <label for=\"token-description-input\"> "
     + alias4(((helper = (helper = lookupProperty(helpers,"TokenDescription") || (depth0 != null ? lookupProperty(depth0,"TokenDescription") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"TokenDescription","hash":{},"data":data,"loc":{"start":{"line":12,"column":46},"end":{"line":12,"column":66}}}) : helper)))
     + " </label>\r\n        <textarea id=\"token-description-input\"></textarea>\r\n    </div>\r\n</div>\r\n<div id=\"bars\">\r\n    \r\n"
     + ((stack1 = lookupProperty(helpers,"each").call(alias1,(depth0 != null ? lookupProperty(depth0,"TokenBar") : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":18,"column":4},"end":{"line":22,"column":13}}})) != null ? stack1 : "")
-    + "\r\n\r\n    <h3 class=\"Token-Controller\">Controlled by: </h3>\r\n    <label for=\"visibletoplayers\">Visible to Players:</label>\r\n    <input type=\"checkbox\" id=\"visible-to-players\">\r\n\r\n</div>\r\n";
+    + "\r\n\r\n    <h3 class=\"Token-Controller\">Controlled by: </h3>\r\n    <label for=\"visibletoplayers\">Visible to Players:</label>\r\n    <input type=\"checkbox\" id=\"visible-to-players\"> \r\n</div>\r\n<button id=\"token-submit\">Submit</button>\r\n";
 },"usePartial":true,"useData":true});
