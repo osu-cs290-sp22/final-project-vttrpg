a1ec1c97e4fb6546ad94b07e73875ca484bc1588
radian628 <70352880+radian628@users.noreply.github.com>
2022-06-07 22:25:16 -0700

    menus now do not open unless a session with a battlemap is open

--- public/canvas.mjs
+++ public/canvas.mjs
@@ -186,7 +186,7 @@ async function testMain() {
         await controller.draw(dragger);
 
 
-        if (shouldPlace) {
+        if (shouldPlace && !(!session.battlemaps || !session.battlemaps[0])) {
             let pixelSpaceCoords = dragger.pixelSpaceToWorldSpace(placePos.x, placePos.y, c);
             nm.setTiles(controller.gridDrawer.activeBattlemap, [
                 {
@@ -214,6 +214,7 @@ async function testMain() {
 
     let brush = document.getElementById("brush");
     brush.addEventListener("click", e => {
+        if (!session.battlemaps || !session.battlemaps[0]) return;
         if (controller.gridDrawer.activeBattlemap == -1) return;
         let imageOptions = {
             images: session.battlemaps[controller.gridDrawer.activeBattlemap].imagePalette
@@ -243,10 +244,15 @@ async function testMain() {
 testMain();
 
 document.getElementById("maps").addEventListener("click", e => {
+    if (!session.battlemaps || !session.battlemaps[0]) return;
     let saveMap = addHTMLStringToDiv(Handlebars.partials.savemap());
     document.body.appendChild(saveMap);
     let saveMapName = document.getElementById("savemap_name");
     let saveMapButton = document.getElementById("savemap_button");
+    document.getElementById("close_button")
+    .addEventListener("click", async (e) => {
+        document.body.removeChild(saveMap);
+    });
     saveMapButton.addEventListener("click", async (e) => {
         let err = false;
         try {
